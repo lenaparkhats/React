d7377db14cfadf4b47f8c3104d78cccc0ebf1466
Lena <lenaparkhats@gmail.com>
2023-08-24 22:11:14 -0400

    Movies

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
-let number = 5;
-const leftBorderWidth = 1;
-console.log(number);
-console.log(leftBorderWidth);
+const numberOfFilms = +prompt("Сколько фильмов вы посмотрели?", "");
+const personalMovieDB ={count: numberOfFilms, movies: {}, actors: {}, genres: [], privat: false};
+let a = prompt("Один из последних фильмов?", ""), b = prompt("Насколько оцените его?", ""),
+c = prompt("Один из последних фильмов?", ""),
+d = prompt("Насколько оцените его?", "");
+    
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
